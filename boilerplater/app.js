b0876f7f4371301e1203a005f232a95b882845d1
anna.arlig <you@example.com>
2022-03-11 11:50:35 +0100

    Boilerplater now adds parent and child directories

--- boilerplater/app.js
+++ boilerplater/app.js
@@ -1,4 +1,21 @@
-const fs = require('fs/promises')
 
-const path = require('path/promises')
-console.log(process.env.PATH);
+const fs = require('fs').promises;
+const path = require('path');
+
+
+(async function main() {
+    try {
+    const parent = process.argv[2];
+    const dirnames  = ['scripts', 'styles', 'assets'];
+      await Promise.all(
+        fs.mkdir(`${parent}`),
+        dirnames.map(dirname => fs.mkdir(`${parent}/${dirname}`).catch(console.error))
+      );
+  
+      // All dirs are created here or errors reported.
+    } catch (err) {
+      console.error(err);
+    }
+
+    console.log(files)
+  })();
